f31b7f3f8230f0330eba9faeffce15f92d6a1d4f
Muhammad Bintang Ramadhan <107125882+cherryjayhawk@users.noreply.github.com>
2024-06-06 17:44:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from os import getenv
 
 if __name__ == "__main__":
 	port = int(getenv("PORT", 8000))
-	uvicorn.run("app.api:app", host="0.0.0.0", port=port, reload=False)
+	uvicorn.run("app.api:app", host="10.168.1.95", port=port, reload=False)
